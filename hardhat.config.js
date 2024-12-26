20d0d8c711f01f3fdb7ab1243612d695928b3484
Charan_1731 <charan23114@gmail.com>
2024-12-26 17:31:03 +0530

    Initial Commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,43 +1,23 @@
-require("@matterlabs/hardhat-zksync-solc");
+require("@nomiclabs/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
-  networks: {
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
-    },
-  },
-  paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
-  },
   solidity: {
-    version: "0.8.17",
+    version: '0.8.9', // Solidity compiler version
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 200, // Optimizer settings for better gas efficiency
       },
+    },
+  },
+  defaultNetwork: 'sepolia', // Default network for deployments
+  networks: {
+    hardhat: {}, // Local Hardhat network for testing
+    sepolia: {
+      url: 'https://rpc.ankr.com/eth_sepolia', // Sepolia RPC URL
+      accounts: process.env.PRIVATE_KEY ? [`0x${process.env.PRIVATE_KEY}`] : [], // Use the private key from the environment variable
+      chainId: 11155111, // Sepolia chain ID
     },
   },
 };
